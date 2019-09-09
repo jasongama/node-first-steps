8c6bcb23092b330f0e2cf29ee98c9cc679a62b49
jasongama <jasongama967@gmail.com>
2019-09-09 15:16:54 +0200

    initial commit

---
+++ greet_jason.js
@@ -0,0 +1,31 @@
+const chalk = require('chalk');
+//import the greet module that is in the current folder
+const greet = require('./greet');
+
+const styledMessage = chalk.bgGreen.black(greet('Jason'));
+console.log(styledMessage)
+
+
+var figlet = require('figlet');
+ 
+figlet('Justin Tarantino', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
+
+figlet.text('Boo!', {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+}, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
